7bdf0f020cb7d6abdfa51703c6ee155dccdb2324
RicardoGmPr <70128085+RicardoGmPr@users.noreply.github.com>
2022-05-19 12:58:05 -0700

    Add files via upload

---
+++ Excepciones/Excepciones/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Excepciones
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+            Console.WriteLine("Ingresa el número 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingresa el número 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine(num1/num2);
+            Console.ReadKey();
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.Write(e.Message);
+            }
+            catch (FormatException e)
+            {
+                Console.Write(e.Message);
+            }
+            finally
+            {
+
+            }
+        }
+    }
+}
